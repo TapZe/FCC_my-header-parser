1ed28c200a9ade961d52bde2ba7e85105c45a1d2
TapZe <nabilmuyassar@Gmail.com>
2024-06-12 21:34:59 +0800

    done

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let json = {"ipaddress": req.ip, "language": req.headers["accept-language"], "software":  req.headers["user-agent"]};
+  res.json(json);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
